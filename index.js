167733ad886a48c38e514da827f6a92b70471eb3
Ubuntu <ubuntu@ip-172-31-35-205.us-west-2.compute.internal>
2016-09-30 09:57:46 +0000

    Part1b V3

--- index.js
+++ index.js
@@ -23,16 +23,20 @@ var fs = require('fs');
 var http = require('http');
 var filename = "index.html";
 
+var port = Number(process.env.PORT || 8080);
+
 var server = http.createServer(function(req, res)
 {
 
-res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, {'Content-Type': 'text/html'});
 
 	fs.readFile(filename, "utf8", function(err, con)
         {
 		if(err)	throw err
-		res.write(data)
+		res.write(con)
 		res.end();
 	});
-}).listen(process.env.PORT || 8080);
+
+}).listen(port);
+
 console.log("End Program");
